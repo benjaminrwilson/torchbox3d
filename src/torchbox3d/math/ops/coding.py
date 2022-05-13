348343126ed0e51fbd77012c50c2e9f56d89653f
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-05-13 15:10:39 -0400

    Remove unused import.

--- src/torchbox3d/math/ops/coding.py
+++ src/torchbox3d/math/ops/coding.py
@@ -11,7 +11,7 @@ from torchbox3d.math.linalg.lie.SO3 import quat_to_yaw, yaw_to_quat
 from torchbox3d.math.ops.index import mgrid
 from torchbox3d.structures.cuboids import Cuboids
 from torchbox3d.structures.outputs import TaskOutputs
-from torchbox3d.structures.regular_grid import RegularGrid, VoxelGrid
+from torchbox3d.structures.regular_grid import RegularGrid
 
 T = TypeVar("T")
 
@@ -91,7 +91,7 @@ def decode(
 
 def _decode_lwh(
     task_outputs_list: List[TaskOutputs],
-    voxel_grid: VoxelGrid,
+    voxel_grid: RegularGrid,
     network_stride: int,
     max_k: int,
     to_nonconsecutive: Tensor,
